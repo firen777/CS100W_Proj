826d3025cebf498053835933bebccf9003ca9241
firen777 <hivemindac1995@gmail.com>
2018-02-14 10:16:30 -0800

    Didn't have time so I peeked at the Solution. Will document it later.

---
+++ Permutation.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+ 
+public class Permutation{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String : ");
+        String str = sc.next();
+        permutation(str);
+    }
+    
+    /**Init Function
+     * @param str String to be permuted
+     */
+    public static void permutation(String str) { 
+        permutation("", str); 
+    }
+
+    /**Actual Recurssive function.
+     * 
+     * @param prefix Charaters that were permuted.
+     * @param str Remaining Characters to be permuted.
+     */
+    private static void permutation(String prefix, String str) {
+        int n = str.length();
+        if (n == 0) System.out.println(prefix);
+        else {
+            for (int i = 0; i < n; i++)
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+        }
+    }
+ 
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CS100W_Proj
+
+Question:
+[How to find all permutations of characters in a String using recursion ? ](http://www.topjavatutorial.com/java/java-programs/top-programming-interview-questions-using-recursion-in-java/)
